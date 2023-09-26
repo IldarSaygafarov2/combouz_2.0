cd94b8a68e5b21d719cbfcf318fde746985bea08
ildar <sayildar17@gmail.com>
2023-09-26 11:55:21 +0500

    added fixes

--- combouz/accounts/models.py
+++ combouz/accounts/models.py
@@ -5,8 +5,8 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField("email address", unique=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField("email address", unique=False)
     phone_number = models.CharField(
         verbose_name="Номер телефона", max_length=15, null=True
     )
